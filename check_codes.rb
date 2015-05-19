e101e97b2f1bc2280fe33e524a8ace9dbe7ba9ea
Andrei Amatuni <andrei.amatuni@gmail.com>
2015-05-18 23:20:55 -0400

    initial commit

---
+++ check_codes.rb
@@ -0,0 +1,29 @@
+require 'Datavyu_API.rb'
+
+begin
+
+	column = "labeled_object_GC"	# set this as necessary
+	
+
+	# checkValidCodes() makes sure that all the codes found in each cell of 
+	# the selected column are valid codes for that specific element. The 
+	# function is called with the value of the "column" variable defined above. 
+	# You need to change this by hand every time you run it on a different column. 
+	
+	checkValidCodes(column, "", 
+					"utterance_type", ["q", "d", "i", "u", "r", "s", "n"], 
+					"object_present", ["y", "n"])
+
+	puts
+
+	col = getColumn(column)
+
+	# Make sure that all the speaker codes are exactly 3 letters long	
+	for cell in col.cells	
+		if cell.speaker.to_s.length != 3
+			puts "Code ERROR (3 letter code required): Var: speaker\tOrdinal: " + cell.ordinal.to_s + "\tVal: " + cell.speaker 
+		end
+	end 
+
+	
+end
---
+++ check_intervals.rb
@@ -0,0 +1,12 @@
+require 'Datavyu_API.rb'
+
+begin 	
+
+	column = "labeled_object_GC"
+
+	col = getColumn(column)
+	
+	# this makes sure all onsets are < offsets
+	scan_for_bad_cells(col)
+	
+end
