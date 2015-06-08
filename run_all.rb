bd75048071d2093495d782e226f7bf998fbf9764
Andrei Amatuni <andrei.amatuni@gmail.com>
2015-06-08 11:26:09 -0400

    more work on run_all.rb

--- run_all.rb
+++ run_all.rb
@@ -1,10 +1,11 @@
 require 'Datavyu_API.rb'
 
 begin
-
+	
 	column = "labeled_object_GC"	# set this as necessary
 
-
+	# Check Codes
+	# 
 	# checkValidCodes() makes sure that all the codes found in each cell of
 	# the selected column are valid codes for that specific element. The
 	# function is called with the value of the "column" variable defined above.
@@ -34,6 +35,8 @@ begin
 
 
 
+	# Check Comments
+	#
 	# Make sure that comment cells don't have any content for
 	# the non-comment codes. Everything except the comment itself
 	# should be "NA". Also makes sure that onset/offset times for
@@ -60,7 +63,8 @@ begin
 
 
 
-
+	# Check Intervals
+	# 
 	# this makes sure all onsets are < offsets
 	#	scan_for_bad_cells(col)
 
@@ -71,5 +75,64 @@ begin
 
 		end
 	end
+	puts "\n\n\n"
+
 	
+
+	# This part runs through the datavyu file
+	# and pulls out all the personal info comments.
+	# It then writes out a file containing the
+	# timestamps of those regions. That output file 
+	# should then be fed into the program that actually 
+	# does the silencing. 
+	#
+	#
+	# These comments should take the form:
+	#
+	#   %com: personal information [audio]: credit card number
+	#
+	# or
+	#
+	#   %com: personal information [video]: butt
+	#
+	#  
+	#  You need to set the output path of the .csv file that 
+	#  will be generated. This is the first line after this comment 
+
+	output_path = "~/desktop/maskregions.csv" # set this as necessary
+
+	output = File.expand_path(output_path) 	
+	col = getColumn(column)
+
+	# arrays containing millisecond onset/offsets for personal information
+	audio_regions = Array.new
+	video_regions = Array.new
+	
+	entry = nil
+	for cell in col.cells
+		entry = cell.object.to_s
+		if (entry.start_with?("%com: personal info"))
+			if (entry.include?("[audio]"))
+				audio_regions.push([cell.onset, cell.offset])
+			elsif (entry.include?("[video]"))
+				video_regions.push([cell.onset, cell.offset])
+			else
+				puts "Malformed personal information comment:  cell#: " + cell.ordinal.to_s
+			end
+		end
+	end
+
+	output_file = File.open(output, "w")
+
+	for region in audio_regions
+			output_file.puts("audio,#{region[0]},#{region[1]}")
+	end
+
+	for region in video_regions
+		output_file.puts("video,#{region[0]},#{region[1]}")
+	end
+
+	output_file.close()
+
+	puts "personal info timestamps written to: " + output_path + "\n\n"
 end
