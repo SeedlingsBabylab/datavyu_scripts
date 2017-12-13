26d92b312ca9c589b9c13a2b01c45021454293ae
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-12-13 09:49:22 -0500

    update merge_reliability to mirror chi check version

--- merge_reliability.rb
+++ merge_reliability.rb
@@ -1,21 +1,24 @@
 require 'Datavyu_API'
 
-# $input_dir = "~/code/work/seedlings/reliability_data/real_runs/video/06/recode_merging"
-# $output_dir = "~/code/work/seedlings/reliability_data/real_runs/video/06/final_out"
+$origin_in = "~/code/work/seedlings/datavyu_scripts/data/reliability_checks/08/merge_back/full_files"
+$recode_in = "~/code/work/seedlings/datavyu_scripts/data/reliability_checks/08/merge_back/converge_out"
+$output_dir = "~/code/work/seedlings/datavyu_scripts/data/reliability_checks/08/merge_back/final_out"
 
-$input_dir = "~/code/work/seedlings/reliability_data/real_runs/video/06/recode_merging"
-$output_dir = "~/code/work/seedlings/reliability_data/real_runs/video/06/final_out"
 
-def merge(in_dir, groups)
+
+def merge(orig_in, reco_in, groups)
   groups.each_value { |files|
     prefix = files["orig"][0..4]
     puts("**********#{prefix}*********")
 
+    if files["consensus"].nil?
+      next
+    end
 
-    $db, $pj = load_db(File.join(in_dir, files["converge"]))
+    $db, $pj = load_db(File.join(reco_in, files["consensus"]))
     conv_col = get_column("recode")
 
-    $db, $pj = load_db(File.join(in_dir, files["orig"]))
+    $db, $pj = load_db(File.join(orig_in, files["orig"]))
 
     cols = get_column_list()
 
@@ -28,20 +31,27 @@ def merge(in_dir, groups)
     end
 
     for cell in conv_col.cells
-      puts(cell.original_ordinal)
+      # puts(cell.original_ordinal)
+      found = false
       for c in orig_column.cells
-        if c.ordinal == cell.original_ordinal.to_i
+        if c.ordinal.to_s == cell.original_ordinal.to_s
+        #   puts("found ordinal: #{c.ordinal}")
           orig_cell = c
+          found = true
         end
       end
       # orig_cell = orig_column.cells[cell.original_ordinal.to_i-1]
-      orig_cell.change_code("object", cell.object)
-      orig_cell.change_code("utterance_type", cell.utterance_type)
-      orig_cell.change_code("object_present", cell.object_present)
-      orig_cell.change_code("speaker", cell.speaker)
-      orig_cell.change_code("onset", cell.onset)
-      orig_cell.change_code("offset", cell.offset)
-      # newcell.change_code("ordinal", cell.ordinal)
+      if !found
+        orig_cell = orig_column.make_new_cell()
+        puts("made a new cell")
+      end
+        # puts(orig_cell.object)
+        orig_cell.change_code("object", cell.object)
+        orig_cell.change_code("utterance_type", cell.utterance_type)
+        orig_cell.change_code("object_present", cell.object_present)
+        orig_cell.change_code("speaker", cell.speaker)
+        orig_cell.change_code("onset", cell.onset)
+        orig_cell.change_code("offset", cell.offset)
     end
 
     set_column(orig_column)
@@ -53,30 +63,36 @@ end
 
 
 begin
-  in_dir = File.expand_path($input_dir)
-  filenames = Dir.new(in_dir).entries
+
+  orig_in = File.expand_path($origin_in)
+  reco_in = File.expand_path($recode_in)
+
+  orig_files = Dir.new(orig_in).entries
+  reco_files = Dir.new(reco_in).entries
+
+  filenames = orig_files + reco_files
 
   groups = Hash.new
   for file in filenames
     if file.end_with? ".opf"
       prefix = file[0..4]
       if groups.has_key?(prefix)
-        if file.include? "converge_rel.opf"
-          groups[prefix]["converge"] = file
+        if file.include? "consensus_relia.opf"
+          groups[prefix]["consensus"] = file
         else
           groups[prefix]["orig"] = file
         end
       else
         groups[prefix] = Hash.new
-        if file.include? "converge_rel.opf"
-          groups[prefix]["converge"] = file
+        if file.include? "consensus_relia.opf"
+          groups[prefix]["consensus"] = file
         else
           groups[prefix]["orig"] = file
         end
       end
     end
   end
-  puts(groups)
-  merge(in_dir, groups)
+  # puts(groups)
+  merge(orig_in, reco_in, groups)
 
 end
