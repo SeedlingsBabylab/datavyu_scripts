e4aa49c084b6189104021621dca1b8b45ff5f21c
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-12-18 12:32:52 -0500

    finish code for merging pho's into original

--- chichecks_merge_into_original.rb
+++ chichecks_merge_into_original.rb
@@ -3,14 +3,15 @@ require 'Datavyu_API'
 
 $merge_table = "~/code/work/seedlings/datavyu_scripts/data/chimerge/chimerge_orig_vs_new.csv"
 $original_in = "~/code/work/seedlings/datavyu_scripts/data/chimerge/original"
+$merge_output = "~/code/work/seedlings/datavyu_scripts/data/chimerge/final_out_merged"
 
 def filter_table(table, file)
     return table.select{|row| row[0] == file}
 end
 
 def old_new_match(old, merg)
-    if merg[0].start_with?("pho")
-        if old.onset.to_s == merg[4] and old.offset.to_s == merg[5]
+    if merg[0].start_with?("%pho")
+        if old.onset.to_s == merg[4] && old.offset.to_s == merg[5] && old.object.start_with?("%pho")
             return true
         else
             return false
@@ -22,6 +23,26 @@ def old_new_match(old, merg)
         return false
     end
 end
+
+def add_cell(col, object, utt_type, present, speaker, onset, offset)
+    new_cell = col.make_new_cell()
+    new_cell.change_code("object", object)
+    new_cell.change_code("utterance_type", utt_type)
+    new_cell.change_code("object_present", present)
+    new_cell.change_code("speaker", speaker)
+    new_cell.change_code("onset", onset)
+    new_cell.change_code("offset", offset)
+end
+
+def change_cell(cell, merge_code)
+    cell.change_code("object", merge_code[0])
+    cell.change_code("utterance_type", merge_code[1])
+    cell.change_code("object_present", merge_code[2])
+    cell.change_code("speaker", merge_code[3])
+    cell.change_code("onset", merge_code[4].to_i)
+    cell.change_code("offset", merge_code[5].to_i)
+end
+
 
 def merge(table, files)
     for file in files
@@ -39,34 +60,60 @@ def merge(table, files)
                 orig_column = get_column(col)
             end
 
-            new_column = create_new_column("labeled_object_new", "object", "utterance_type", "object_present", "speaker", "original_ordinal")
+            new_column = create_new_column("labeled_object_new", "object", "utterance_type", "object_present", "speaker")
 
+            # port all the codes that already exist in the original, 
+            # matching with their corresponding cell in the merge table
             for orig_cell in orig_column.cells
                 found = false
                 for code in entries
-                    # puts(code)
-                    # puts(code.length)
-                    # puts(code[1])
-                    # puts(code[2].split("_"))
+                    # n_code = code[2].strip()
+                    if !code[2]
+                        break
+                    end
                     orig_code = code[1].split("_")
                     merg_code = code[2].split("_")
                     if old_new_match(orig_cell, merg_code)
                         found = true
-                        puts("hurray")
+                        add_cell(new_column, merg_code[0], merg_code[1], 
+                                merg_code[2], merg_code[3], merg_code[4].to_i, 
+                                merg_code[5].to_i)
                     end
                 end
 
                 if !found
-                    merg_cell = orig_column.make_new_cell()
-                    merg_cell.change_code("object", cell)
-                    merg_cell.change_code("utterance_type", "XXX")
-                    merg_cell.change_code("object_present", "XXX")
-                    merg_cell.change_code("speaker", "XXX")
-                    puts("new not found")
-                end
-
-                    
+                    add_cell(new_column, orig_cell.object, orig_cell.utterance_type,
+                        orig_cell.object_present, orig_cell.speaker, orig_cell.onset,
+                        orig_cell.offset)
+                end  
             end
+
+            # merge all the pho's
+            for code in entries
+                if code[2]
+                    if code[2].start_with?("%pho")
+                        merg_code = code[2].split("_")
+                        found = false
+                        for cell in new_column.cells
+                            if old_new_match(cell, merg_code) # old %pho that snuck in because timestamp matched
+                                found = true
+                                change_cell(cell, merg_code)
+                            end
+                        end
+                        if !found # new pho that needs to be added
+                            add_cell(new_column, merg_code[0], merg_code[1], 
+                                    merg_code[2], merg_code[3], merg_code[4].to_i, 
+                                    merg_code[5].to_i)
+                        end
+                    end
+                end
+            end
+
+            for col in cols
+                delete_variable(col)
+            end
+            set_column(new_column)
+            save_db(File.join(File.expand_path($merge_output), "#{f_prefix}_sparse_code.opf"))
         end
 
 
