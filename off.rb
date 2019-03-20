ad59bee6480dd5188abfbf1c6ed7abacb131c85d
Estelle He <wh103@duke.edu>
2019-03-20 17:05:44 -0400

    fix off

--- off.rb
+++ off.rb
@@ -9,20 +9,27 @@ begin
   csv_data = CSV.read(csv_file)
   col_name = csv_data[0][0].split('.')[0]
   puts col_name
+  code_list = Array.new
 
   new_column = create_new_column(col_name)
   for n in 0..(csv_data[0].length-1) do
     code = csv_data[0][n].split('.')[1]
     new_column.add_code(code)
-    for k in 1..(csv_data.length-1) do
-      newcell = new_column.make_new_cell()
-      newcell.change_code(code, csv_data[k][n])
-      puts csv_data[k][n]
-    end
-    puts code
-    set_column(new_column)
-    puts code
+    code_list << code
   end
+
+  for k in 1..(csv_data.length-1) do
+    newcell = new_column.make_new_cell()
+    for n in 0..(csv_data[0].length-1) do
+      if (code_list[n] == "ordinal") or (code_list[n] == "onset") or (code_list[n] == "offset") then
+        newcell.change_code(code_list[n], csv_data[k][n].to_i)
+      else
+        newcell.change_code(code_list[n], csv_data[k][n])
+      end
+    end
+  end
+
+  setColumn(new_column)
 
   save_db(File.join(File.expand_path(output_folder), csv_path.sub!(".csv", ".opf")))
 
